a2c29a2e3b651e0877b6dde5b212d964000f473e
mayank123813 <kambojmayank04@gmail.com>
2026-09-19 11:33:42 +0530

    implemented chrome options

--- src/test/java/com/opencart/automation/factory/WebDriverFactory.java
+++ src/test/java/com/opencart/automation/factory/WebDriverFactory.java
@@ -4,8 +4,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class WebDriverFactory {
 
@@ -18,12 +21,23 @@ public class WebDriverFactory {
 
         if (type.equalsIgnoreCase("chrome")) {
 
+            ChromeOptions options = new ChromeOptions();
+
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+
             logger.info("Starting Chrome browser");
             return new ChromeDriver();
 
         }
 
         if (type.equalsIgnoreCase("firefox")) {
+            FirefoxOptions options = new FirefoxOptions();
+
+            options.addArguments("-headless");
+            options.addArguments("--width=1920");
+            options.addArguments("--height=1080");
 
             logger.info("Starting Firefox browser");
             return new FirefoxDriver();
@@ -31,6 +45,12 @@ public class WebDriverFactory {
         }
 
         if (type.equalsIgnoreCase("edge")) {
+
+            EdgeOptions options = new EdgeOptions();
+
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
 
             logger.info("Starting Edge browser");
             return new EdgeDriver();
